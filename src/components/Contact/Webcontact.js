cb3d348daf68e59c19cdffcc9458374fb345f9aa
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-05-17 10:42:39 +0530

    Update Webcontact.js

--- src/components/Contact/Webcontact.js
+++ src/components/Contact/Webcontact.js
@@ -93,6 +93,7 @@ const payload = {
 			  data: payload, // you are sending body instead
 			 
 			}).then(function(response) {
+				console.log(response);
 				console.log(response.ResponseCode);
 				if(response.ResponseCode == 200){
 					window.location.href = "https://www.qltech.com.au/thank-you/";
