5ebf7ceb090422dee646d59dbfda86af3a01e115
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-08-05 17:14:38 +0530

    Update uae-website-creation.js

--- src/pages/uae-website-creation.js
+++ src/pages/uae-website-creation.js
@@ -62,7 +62,8 @@ class FilterGrid extends React.Component {
 				
 			console.log(response.data.country_code2);
 				if(response.data.country_code2 != "IN" && response.data.country_code2 != "AE"){
-					window.location.href = "https://www.qltech.com.au";
+					//window.location.href = "https://www.qltech.com.au";
+					console.log("here in cond");
 					
 				}
 				
