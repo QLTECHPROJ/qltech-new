599ad9b597e798353be5775651df09bb42f8ea36
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-08-06 17:34:17 +0530

    Update Webcontact.js

--- src/components/Contact/Webcontact.js
+++ src/components/Contact/Webcontact.js
@@ -121,7 +121,7 @@ const payload = {
 	  
 	  
 							<div className="row justify-content-center">
-								<div className="col-lg-8 col-md-10">
+								<div className="col-lg-10 col-md-10">
 									 <div className="row">
 										
 										<div className="col-md-12 mb-4">
