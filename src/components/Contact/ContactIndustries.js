bf914e73fb9ae9ac96cc78a62ad755b7ae390d07
ChintanQL <86038331+ChintanQL@users.noreply.github.com>
2024-08-22 11:22:38 +0530

    Update ContactIndustries.js

--- src/components/Contact/ContactIndustries.js
+++ src/components/Contact/ContactIndustries.js
@@ -173,7 +173,7 @@ class ContactIndustries extends React.Component{
 										<div className="col-md-12 mt-3">
 											<ReCAPTCHA
 												ref={recaptchaRef}
-												sitekey=" 6Lc5jjEUAAAAAI1yf3CfFogxqiok5pt7wcF7_SKJ"
+												sitekey="6LfliiwqAAAAACFOqWuUVddqUrHE2DQUbaxTRNKk"
 											/>
 										</div>
 										
